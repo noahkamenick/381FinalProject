5f44bae6402c546ad47ce46a7691708b3cacadca
noahkamenick <noahkamenick@gmail.com>
2021-12-06 02:39:34 +0000

    fixed code for paramiko

--- myparamiko.py
+++ myparamiko.py
@@ -23,10 +23,8 @@ if ssh_client.get_transport().is_active():
 
 shell = ssh_client.invoke_shell() #Create Shell object
 shell.send('enable\n')
-shell.send('password')
-shell.send('\n')
 shell.send('terminal length 0\n')
-shell.send('show int br\n')
+shell.send('show ip int br\n')
 
 
 time.sleep(3)
